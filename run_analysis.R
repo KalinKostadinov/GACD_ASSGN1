a18d150896a8c3d6e03264978d412bda2dd3dcac
KalinKostadinov <kalin_lefterov@yahoo.com>
2014-11-23 14:58:42 -0500

    Fixing Errors
    Who would think that unsaved changes in RStudio are not synced to the
    saved file on disk.

--- run_analysis.R
+++ run_analysis.R
@@ -80,6 +80,7 @@ col_idx<-append(1:2,col_idx)
 meanstd_data_set=full_data_set[,col_idx]
 
 rm(full_data_set)
+rm(features)
 
 #check what we got
 #head(meanstd_data_set)  #look's ok
@@ -102,9 +103,35 @@ x<-names(meanstd_data_set)
 x<-gsub("[0-9]|\\(|\\)| ","",x)
 names(meanstd_data_set)<-x
 rm(x)
+#check what we go
+#names(meanstd_data_set)
 #End of Step4
 
 
 #5 From the data set in step 4, creates a second, independent tidy data set 
 #       with the average of each variable for each activity and each subject.
+#will use aggregate with FUN=mean; melt&dcast seems like an overkill
 
+#split the set into part to be aggregated and part to aggregate by
+measurements_part=meanstd_data_set[,3:68]
+subjects_part=meanstd_data_set[,"SubjectCode"]
+activities_part=meanstd_data_set[,"ActivityCode"]
+
+tidy_data_set<-aggregate(measurements_part,by=list(subjects_part,activities_part),FUN=mean)
+
+rm(measurements_part)
+rm(subjects_part)
+rm(activities_part)
+  
+#relabel the columns
+names(tidy_data_set)<-paste("AVG-",names(tidy_data_set))
+names(tidy_data_set)[1]<-"SubjectCode"
+names(tidy_data_set)[2]<-"Acvtivity"
+
+#produce the output
+
+write.table(tidy_data_set,"./Data/GACD ASSGN1 UCI HAR Dataset/tidy_data_set.txt",row.names=FALSE)
+#check what we got
+#dim(tidy_data_set)  #180 obs (6act X 30 subj) of 66 (2+64)
+#tidy_data_set[120:125,1:5]
+#End of Step5
